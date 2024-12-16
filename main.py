bac8291d0801d022a6ce1fe5acb3c065c2ec033d
vnutkin <vl_utkin@yahoo.com>
2024-12-16 22:15:41 +0300

    class Orbit_point
    class Orbit_point

---
+++ main.py
@@ -0,0 +1,11 @@
+class Orbit_point():
+    def __init__(self,r_planet,h_orbit,phi,x_abs,y_abs,width_screen,heigth_screen,x_screen,y_screen):
+        self.r_planet = r_planet
+        self.h_orbit = h_orbit
+        self.phi = phi
+        self.x_abs = x_abs
+        self.y_abs = y_abs
+        self.width_screen = width_screen
+        self.heigth_screen = heigth_screen
+        self.x_screen = x_screen
+        self.y_screen = y_screen
